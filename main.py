eead97110d5f26a7441457b71739a1cecba476ac
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-08-08 10:09:06 +0300

    added logo and more page

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import distribution,relationship,home,heatmap
+import distribution,relationship,home,heatmap,stats
+
+#todo:
+    #add the logo
+    #try to match to the https://dashboard.vodana.health/ site
+
+#setup the background color
+#import the css file into the streamlit app
+# with open('./main-css.css') as f:
+#     css=f.read()
+
+# st.markdown(f'<style>{css}</style>',unsafe_allow_html=True)
+
+
+#lets assign the logo
+
+st.image(
+     image="ayder_image.jpg",
+      icon_image=None
+)
 
 #set the page configuration
 st.set_page_config(
@@ -29,7 +48,7 @@ class MultiPage:
                 menu_icon='chat-text-fill',
                 default_index=0,
                 styles={
-                    "container":{"padding":"10px","background-color":"#000000"},
+                    "container":{"padding":"10px","background-color":"#4B0082"},
                     "icon":{"color":"white","font-size":"23px"},
                     "nav-link":{"color":"white","font-size":"23px","text-align":"10px","--hover-color":"#228B22"},
                     "nav-link-selected":{"background-color":"#006400"}
@@ -50,6 +69,7 @@ app.add_app("Home",home.app)
 app.add_app("distribution",distribution.app)
 app.add_app("relationship",relationship.app)
 app.add_app("heatmap",heatmap.app)
+app.add_app("stats",stats.app)
 
 app.run()
 
--- relationship.py
+++ relationship.py
@@ -9,7 +9,7 @@ df=pd.read_csv(datapath)
 df.columns=[col for col in string.ascii_lowercase[:len(df.columns)]]
 
 def app():
-    x_col=st.selectbox('select x axis',df.columns)
-    y_col=st.selectbox('select y axis',df.columns)
+    x_col=st.selectbox('select x axis',df.columns,0)
+    y_col=st.selectbox('select y axis',df.columns,1)
 
     st.bar_chart(df[[x_col,y_col]])
